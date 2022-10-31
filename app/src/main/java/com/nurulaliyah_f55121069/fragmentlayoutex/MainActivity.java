7912a1899f96ca03b956c53e90ddaaed8491459c
nurulalyh <nurulaliyah2303@gmail.com>
2022-11-01 03:54:53 +0800

    Initial commit

---
+++ app/src/main/java/com/nurulaliyah_f55121069/fragmentlayoutex/MainActivity.java
@@ -0,0 +1,34 @@
+package com.nurulaliyah_f55121069.fragmentlayoutex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appBar_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        // Adding Fragment
+        adapter.AddFragment(new FragmentHome(), "Home");
+        adapter.AddFragment(new FragmentExplore(), "Explore");
+        adapter.AddFragment(new FragmentAccount(), "Account");
+        //Adding Adapter
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
